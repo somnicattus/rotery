c6d92a2a1de91d917d2094bf163cb25cb7f14081
somnicattus <somnicattus@gmail.com>
2025-01-10 22:30:46 +0900

    ♻️ refactor `unique` using `filter`

--- src/operations/filters/unique.ts
+++ src/operations/filters/unique.ts
@@ -1,37 +1,15 @@
-import { isIterable } from '../../controls/guards.js';
 import type { Series, SyncSeries } from '../../controls/types.js';
+import { filter } from './filter.js';
 
 /** Creates a series without duplicated elements. */
 export namespace unique {
-    export function* sync<T>(input: SyncSeries<T>): Generator<T> {
+    export function sync<T>(input: SyncSeries<T>): Generator<T> {
         const set = new Set<T>();
-        for (const value of input) {
-            if (!set.has(value)) {
-                set.add(value);
-                yield value;
-            }
-        }
+        return filter.sync(input, value => (set.has(value) ? false : (set.add(value), true)));
     }
 
-    // eslint-disable-next-line complexity -- need blocks to yield values
-    export async function* async<T>(input: Series<T>): AsyncGenerator<Awaited<T>> {
+    export function async<T>(input: Series<T>): AsyncGenerator<Awaited<T>> {
         const set = new Set<Awaited<T>>();
-        const awaited = await input;
-        if (isIterable(awaited)) {
-            for (const value of awaited) {
-                const v = await value;
-                if (!set.has(v)) {
-                    set.add(v);
-                    yield v;
-                }
-            }
-        } else {
-            for await (const value of awaited) {
-                if (!set.has(value)) {
-                    set.add(value);
-                    yield value;
-                }
-            }
-        }
+        return filter.async(input, value => (set.has(value) ? false : (set.add(value), true)));
     }
 }
