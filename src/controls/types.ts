9a064104057ea0111ab850af8ad814874f1e6a08
somnicattus <somnicattus@gmail.com>
2025-12-29 21:38:11 +0900

    Add tsdoc in types.ts for improved clarity

--- src/controls/types.ts
+++ src/controls/types.ts
@@ -1,15 +1,37 @@
+/**
+ * A type that represents a static series of values.
+ */
 export type StaticSeries<T> = readonly T[] | ReadonlySet<T>;
+
+/**
+ * A type that represents a synchronous series of values.
+ */
 export type SyncSeries<T> = StaticSeries<T> | IterableIterator<T>;
+
+/**
+ * A type that can be either a synchronous series or an asynchronous series.
+ */
 export type Series<T> = MaybePromise<
     SyncSeries<MaybePromise<T>> | AsyncIterableIterator<MaybePromise<T>>
 >;
 
+/**
+ * A type that can be either T or a Promise resolving to T.
+ */
 export type MaybePromise<T> = T | Promise<T>;
 
+/**
+ * An array with a variant length up to L.
+ */
 export type VariantLengthArray<T, L extends number, A extends T[] = []> = number extends L
     ? T[]
     : A['length'] extends L
       ? A
       : A | VariantLengthArray<T, L, [T, ...A]>;
 
+/**
+ * An array with a fixed chunk size.
+ *
+ * Tails can be shorter if the total number of elements is not divisible by the chunk size.
+ */
 export type Chunked<T, L extends number> = VariantLengthArray<T, L, [T]>;
