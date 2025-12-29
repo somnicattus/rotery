4a8b16033ccf5c4849df8e351d4499b2ed5bdcb4
somnicattus <somnicattus@gmail.com>
2025-12-29 21:46:25 +0900

    Add additional overloads to compose function for up to 15 functions

--- src/compositions/compose.ts
+++ src/compositions/compose.ts
@@ -53,6 +53,122 @@ export function compose<A, B, C, D, E, F, G, H>(
     fn7: (value: G) => H,
 ): (value: A) => H;
 
+export function compose<A, B, C, D, E, F, G, H, I>(
+    fn1: (value: A) => B,
+    fn2: (value: B) => C,
+    fn3: (value: C) => D,
+    fn4: (value: D) => E,
+    fn5: (value: E) => F,
+    fn6: (value: F) => G,
+    fn7: (value: G) => H,
+    fn8: (value: H) => I,
+): (value: A) => I;
+
+export function compose<A, B, C, D, E, F, G, H, I, J>(
+    fn1: (value: A) => B,
+    fn2: (value: B) => C,
+    fn3: (value: C) => D,
+    fn4: (value: D) => E,
+    fn5: (value: E) => F,
+    fn6: (value: F) => G,
+    fn7: (value: G) => H,
+    fn8: (value: H) => I,
+    fn9: (value: I) => J,
+): (value: A) => J;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K>(
+    fn01: (value: A) => B,
+    fn02: (value: B) => C,
+    fn03: (value: C) => D,
+    fn04: (value: D) => E,
+    fn05: (value: E) => F,
+    fn06: (value: F) => G,
+    fn07: (value: G) => H,
+    fn08: (value: H) => I,
+    fn09: (value: I) => J,
+    fn10: (value: J) => K,
+): (value: A) => K;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L>(
+    fn01: (value: A) => B,
+    fn02: (value: B) => C,
+    fn03: (value: C) => D,
+    fn04: (value: D) => E,
+    fn05: (value: E) => F,
+    fn06: (value: F) => G,
+    fn07: (value: G) => H,
+    fn08: (value: H) => I,
+    fn09: (value: I) => J,
+    fn10: (value: J) => K,
+    fn11: (value: K) => L,
+): (value: A) => L;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M>(
+    fn01: (value: A) => B,
+    fn02: (value: B) => C,
+    fn03: (value: C) => D,
+    fn04: (value: D) => E,
+    fn05: (value: E) => F,
+    fn06: (value: F) => G,
+    fn07: (value: G) => H,
+    fn08: (value: H) => I,
+    fn09: (value: I) => J,
+    fn10: (value: J) => K,
+    fn11: (value: K) => L,
+    fn12: (value: L) => M,
+): (value: A) => M;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
+    fn01: (value: A) => B,
+    fn02: (value: B) => C,
+    fn03: (value: C) => D,
+    fn04: (value: D) => E,
+    fn05: (value: E) => F,
+    fn06: (value: F) => G,
+    fn07: (value: G) => H,
+    fn08: (value: H) => I,
+    fn09: (value: I) => J,
+    fn10: (value: J) => K,
+    fn11: (value: K) => L,
+    fn12: (value: L) => M,
+    fn13: (value: M) => N,
+): (value: A) => N;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
+    fn01: (value: A) => B,
+    fn02: (value: B) => C,
+    fn03: (value: C) => D,
+    fn04: (value: D) => E,
+    fn05: (value: E) => F,
+    fn06: (value: F) => G,
+    fn07: (value: G) => H,
+    fn08: (value: H) => I,
+    fn09: (value: I) => J,
+    fn10: (value: J) => K,
+    fn11: (value: K) => L,
+    fn12: (value: L) => M,
+    fn13: (value: M) => N,
+    fn14: (value: N) => O,
+): (value: A) => O;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
+    fn01: (value: A) => B,
+    fn02: (value: B) => C,
+    fn03: (value: C) => D,
+    fn04: (value: D) => E,
+    fn05: (value: E) => F,
+    fn06: (value: F) => G,
+    fn07: (value: G) => H,
+    fn08: (value: H) => I,
+    fn09: (value: I) => J,
+    fn10: (value: J) => K,
+    fn11: (value: K) => L,
+    fn12: (value: L) => M,
+    fn13: (value: M) => N,
+    fn14: (value: N) => O,
+    fn15: (value: O) => P,
+): (value: A) => P;
+
 export function compose(...functions: ReadonlyArray<(value: unknown) => unknown>): unknown {
     // @ts-expect-error (ts2556) pipe's type definition is overloaded for users
     return value => pipe(value, ...functions);
