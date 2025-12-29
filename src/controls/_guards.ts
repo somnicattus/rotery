bbc002253d4dc8e541023fd821afe7516c7e95f3
somnicattus <somnicattus@gmail.com>
2025-12-29 22:14:57 +0900

    Refactor _isIterable function to ensure proper iterable check

--- src/controls/_guards.ts
+++ src/controls/_guards.ts
@@ -2,4 +2,5 @@ import type { Series } from './types.js';
 
 export const _isIterable = (
     value: Awaited<Series<unknown>>,
-): value is Awaited<Series<unknown>> & Iterable<unknown> => Symbol.iterator in value;
+): value is Awaited<Series<unknown>> & Iterable<unknown> =>
+    Symbol.iterator in value && typeof value[Symbol.iterator] === 'function';
